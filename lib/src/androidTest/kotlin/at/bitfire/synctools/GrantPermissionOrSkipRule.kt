33cf3ce6a83432e5818d268eca939b52010583b2
Ricki Hirner <hirner@bitfire.at>
2025-06-09 12:58:14 +0200

    Update copyright headers and AUTHORS file (#10)

--- lib/src/androidTest/kotlin/at/bitfire/synctools/GrantPermissionOrSkipRule.kt
+++ lib/src/androidTest/kotlin/at/bitfire/synctools/GrantPermissionOrSkipRule.kt
@@ -1,5 +1,5 @@
 /*
- * This file is part of ical4android which is released under GPLv3.
+ * This file is part of bitfireAT/synctools which is released under GPLv3.
  * Copyright © All Contributors. See the LICENSE and AUTHOR files in the root directory for details.
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
