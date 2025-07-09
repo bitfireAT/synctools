c1da3442a246b0817c389338f3e1af98e39f72c1
Ricki Hirner <hirner@bitfire.at>
2025-07-09 11:45:49 +0200

    Revert commenting out "set duration to null"

--- lib/src/main/kotlin/at/bitfire/ical4android/AndroidEvent.kt
+++ lib/src/main/kotlin/at/bitfire/ical4android/AndroidEvent.kt
@@ -244,7 +244,7 @@ class AndroidEvent(
                 if (duration is Duration)
                     duration = Period.ofDays(duration.toDays().toInt())
                 tsEnd = (startDate + duration).toEpochDay() * TimeApiExtensions.MILLIS_PER_DAY
-                //duration = null
+                duration = null
             }
 
             if (tsEnd != null) {
@@ -284,7 +284,7 @@ class AndroidEvent(
                 // Some servers have problems with DURATION, so we always generate DTEND.
                 val zonedStart = dtStartDateTime.toZonedDateTime()
                 tsEnd = (zonedStart + duration).toInstant().toEpochMilli()
-                //duration = null
+                duration = null
             }
 
             if (tsEnd != null) {
