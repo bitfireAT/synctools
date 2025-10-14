606bd211525e446c1abfa314177303b58fc4fef5
Sunik Kupfer <kupfer@bitfire.at>
2025-10-14 13:24:10 +0200

    Add test to assert presence of AndroidCompatTimeZoneRegistry

---
+++ lib/src/test/kotlin/at/bitfire/ical4android/Ical4jConfigurationTest.kt
@@ -0,0 +1,27 @@
+/*
+ * This file is part of bitfireAT/synctools which is released under GPLv3.
+ * Copyright © All Contributors. See the LICENSE and AUTHOR files in the root directory for details.
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package at.bitfire.ical4android
+
+import net.fortuna.ical4j.model.TimeZoneRegistryFactory
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+
+import org.junit.Test
+
+class Ical4jConfigurationTest {
+
+    @Test
+    fun testTimeZoneRegistryFactoryConfigured() {
+        val registry = TimeZoneRegistryFactory.getInstance().createRegistry()
+        assertEquals(
+            "at.bitfire.ical4android.AndroidCompatTimeZoneRegistry",
+            registry.javaClass.name
+        )
+    }
+
+}
