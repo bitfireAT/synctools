c03bf64908bf085304d69dcec0f301b7653e36b1
Sunik Kupfer <kupfer@bitfire.at>
2025-12-02 13:37:44 +0100

    Always add DTEND to vevent

--- lib/src/main/kotlin/at/bitfire/synctools/mapping/calendar/handler/EndTimeHandler.kt
+++ lib/src/main/kotlin/at/bitfire/synctools/mapping/calendar/handler/EndTimeHandler.kt
@@ -8,9 +8,12 @@ package at.bitfire.synctools.mapping.calendar.handler
 
 import android.content.Entity
 import android.provider.CalendarContract.Events
+import at.bitfire.synctools.util.AndroidTimeUtils
 import net.fortuna.ical4j.model.TimeZoneRegistry
 import net.fortuna.ical4j.model.component.VEvent
 import net.fortuna.ical4j.model.property.DtEnd
+import java.time.Duration
+import java.time.Period
 import java.util.logging.Logger
 
 class EndTimeHandler(
@@ -22,19 +25,29 @@ class EndTimeHandler(
 
     override fun process(from: Entity, main: Entity, to: VEvent) {
         val values = from.entityValues
-        val allDay = (values.getAsInteger(Events.ALL_DAY) ?: 0) != 0
 
-        // Skip if DTEND is not set – then usually DURATION is set; however it's also OK to have neither DTEND nor DURATION in a VEVENT.
-        val tsEnd = values.getAsLong(Events.DTEND) ?: return
-
-        // Also skip if DTEND is not after DTSTART (not allowed in iCalendar)
+        // Skip if start is not set
         val tsStart = values.getAsLong(Events.DTSTART) ?: return
-        if (tsEnd <= tsStart) {
-            logger.warning("Ignoring DTEND=$tsEnd that is not after DTSTART=$tsStart")
+
+        // Skip if valid DURATION (non-zero and positive) is set
+        // (even if its invalid, since we never want DURATION _and_ DTSTART)
+        if (values.getAsString(Events.DURATION) != null)
             return
+
+        // Default to DTSTART value if DTEND is not set and DURATION is not present.
+        // This is a workaround for iCloud that is always applied; it seems not to bother other servers.
+        // Normally it's OK to have neither DTEND nor DURATION in a VEVENT with DTSTART, but iCloud
+        // rejects that with with a 404. See davx5-ose#1859
+        var tsEnd = values.getAsLong(Events.DTEND) ?: tsStart
+
+        // Also default to the DTSTART value if DTEND is not after DTSTART (which is not allowed in iCalendar)
+        if (tsEnd <= tsStart) {
+            logger.warning("DTEND=$tsEnd is not after DTSTART=$tsStart, setting DTEND=$tsStart")
+            tsEnd = tsStart
         }
 
         // DATE or DATE-TIME according to allDay
+        val allDay = (values.getAsInteger(Events.ALL_DAY) ?: 0) != 0
         val end = AndroidTimeField(
             timestamp = tsEnd,
             timeZone = values.getAsString(Events.EVENT_END_TIMEZONE)
