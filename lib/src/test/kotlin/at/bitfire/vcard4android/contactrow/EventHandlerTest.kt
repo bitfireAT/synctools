5fb54ec88c06d499cc4ce78aa2e0228b537286dd
Arnau Mora <arnyminerz@proton.me>
2025-12-05 13:54:48 +0000

    Add support for ZonedDateTime in `EventHandler` (#136)
    * Add support for ZonedDateTime in `EventHandler`
    
    * Optimize imports
    
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>
    
    * Adjust date
    
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>
    
    * Typo
    
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>
    
    * Add `parseStartDate`
    
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>
    
    * Added tests for all datetime formats from AOSP
    
    * Remove debug
    
    * Update link
    
    * Move partial date parsing logic to function
    
    * Cleanup
    
    * Remove continue
    
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>
    
    * Add `@VisibleForTesting`
    
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>
    
    * Replace vars
    
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>
    
    * Optimization of stripping fractions of seconds
    
    * Comments / KDoc
    
    * Reorder methods
    
    * Reorder tests
    
    ---------
    
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>
    Co-authored-by: Ricki Hirner <hirner@bitfire.at>

--- lib/src/test/kotlin/at/bitfire/vcard4android/contactrow/EventHandlerTest.kt
+++ lib/src/test/kotlin/at/bitfire/vcard4android/contactrow/EventHandlerTest.kt
@@ -19,9 +19,14 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import java.time.LocalDate
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
 
 @RunWith(RobolectricTestRunner::class)
 class EventHandlerTest {
+
+    // Tested date formats are as provided by Android AOSP Contacts App
+    // https://android.googlesource.com/platform/packages/apps/Contacts/+/refs/tags/android-13.0.0_r49/src/com/android/contacts/util/CommonDateUtils.java
 
     @Test
     fun testStartDate_Empty() {
@@ -35,7 +40,7 @@ class EventHandlerTest {
     }
 
     @Test
-    fun testStartDate_Full() {
+    fun testStartDate_FULL_DATE_FORMAT() {
         val contact = Contact()
         EventHandler.handle(ContentValues().apply {
             put(Event.START_DATE, "1984-08-20")
@@ -47,12 +52,37 @@ class EventHandlerTest {
     }
 
     @Test
-    fun testStartDate_Partial() {
+    fun testStartDate_DATE_AND_TIME_FORMAT() {
+        val contact = Contact()
+        EventHandler.handle(ContentValues().apply {
+            put(Event.START_DATE, "1953-10-15T23:10:12.345Z")
+        }, contact)
+        assertEquals(
+            OffsetDateTime.of(1953, 10,  15, 23, 10, 12, 345_000_000, ZoneOffset.UTC),
+            contact.customDates[0].property.date
+        )
+    }
+
+    @Test
+    fun testStartDate_NO_YEAR_DATE_FORMAT() {
         val contact = Contact()
         EventHandler.handle(ContentValues().apply {
             put(Event.START_DATE, "--08-20")
         }, contact)
         assertEquals(PartialDate.parse("--0820"), contact.customDates[0].property.partialDate)
+    }
+
+    @Test
+    fun testStartDate_NO_YEAR_DATE_AND_TIME_FORMAT() {
+        val contact = Contact()
+        EventHandler.handle(ContentValues().apply {
+            put(Event.START_DATE, "--08-20T23:10:12.345Z")
+        }, contact)
+        // Note that nanoseconds are stripped in PartialDate
+        assertEquals(
+            PartialDate.builder().month(8).date(20).hour(23).minute(10).second(12).offset(ZoneOffset.UTC).build(),
+            contact.customDates[0].property.partialDate
+        )
     }
 
 
@@ -104,6 +134,43 @@ class EventHandlerTest {
         assertNull(contact.anniversary)
         assertNull(contact.birthDay)
         assertFalse(contact.customDates.isEmpty())
+    }
+
+
+    // test parser methods
+
+    @Test
+    fun test_parseFullDate_ISO_DATE_AND_TIME_FORMAT_DateTime() {
+        assertEquals(
+            OffsetDateTime.of(1953, 10,  15, 23, 10, 0, 0, ZoneOffset.UTC),
+            EventHandler.parseFullDate("1953-10-15T23:10:00Z")
+        )
+    }
+
+    @Test
+    fun test_parseFullDate_FULL_DATE_FORMAT_Date() {
+        assertEquals(
+            LocalDate.of(1953, 10,  15),
+            EventHandler.parseFullDate("1953-10-15")
+        )
+    }
+
+
+    @Test
+    fun test_parsePartialDate_NO_YEAR_DATE_FORMAT() {
+        assertEquals(
+            PartialDate.builder().month(10).date(15).build(),
+            EventHandler.parsePartialDate("--10-15")
+        )
+    }
+
+    @Test
+    fun test_parsePartialDate_NO_YEAR_DATE_AND_TIME_FORMAT() {
+        // Partial date does not support nanoseconds, so they will be removed
+        assertEquals(
+            PartialDate.builder().month(8).date(20).hour(23).minute(10).second(12).offset(ZoneOffset.UTC).build(),
+            EventHandler.parsePartialDate("--08-20T23:10:12.345Z")
+        )
     }
 
 }
