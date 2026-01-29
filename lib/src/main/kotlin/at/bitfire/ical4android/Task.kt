20ae9be34d1eb4b99f69f5413b319ee867a5ebe2
Sunik Kupfer <kupfer@bitfire.at>
2026-01-29 14:35:21 +0100

    Extract task reading/parsing from data class

--- lib/src/main/kotlin/at/bitfire/ical4android/Task.kt
+++ lib/src/main/kotlin/at/bitfire/ical4android/Task.kt
@@ -8,11 +8,9 @@ package at.bitfire.ical4android
 
 import androidx.annotation.IntRange
 import at.bitfire.ical4android.util.DateUtils
-import at.bitfire.synctools.exception.InvalidICalendarException
 import at.bitfire.synctools.icalendar.Css3Color
 import net.fortuna.ical4j.data.CalendarOutputter
 import net.fortuna.ical4j.model.Calendar
-import net.fortuna.ical4j.model.Component
 import net.fortuna.ical4j.model.DateTime
 import net.fortuna.ical4j.model.Property
 import net.fortuna.ical4j.model.TextList
@@ -26,7 +24,6 @@ import net.fortuna.ical4j.model.property.Comment
 import net.fortuna.ical4j.model.property.Completed
 import net.fortuna.ical4j.model.property.Created
 import net.fortuna.ical4j.model.property.Description
-import net.fortuna.ical4j.model.property.DtStamp
 import net.fortuna.ical4j.model.property.DtStart
 import net.fortuna.ical4j.model.property.Due
 import net.fortuna.ical4j.model.property.Duration
@@ -47,15 +44,19 @@ import net.fortuna.ical4j.model.property.Summary
 import net.fortuna.ical4j.model.property.Uid
 import net.fortuna.ical4j.model.property.Url
 import net.fortuna.ical4j.model.property.Version
-import java.io.IOException
 import java.io.OutputStream
-import java.io.Reader
 import java.net.URI
 import java.net.URISyntaxException
 import java.util.LinkedList
 import java.util.logging.Level
 import java.util.logging.Logger
 
+/**
+ * Data class representing a task
+ *
+ * - as it is extracted from an iCalendar or
+ * - as it should be generated into an iCalendar.
+ */
 data class Task(
     var createdAt: Long? = null,
     var lastModified: Long? = null,
@@ -98,98 +99,6 @@ data class Task(
 
         private val logger
             get() = Logger.getLogger(Task::class.java.name)
-
-        /**
-         * Parses an iCalendar resource, applies [at.bitfire.synctools.icalendar.validation.ICalPreprocessor] to increase compatibility
-         * and extracts the VTODOs.
-         *
-         * @param reader where the iCalendar is taken from
-         *
-         * @return array of filled [Task] data objects (may have size 0)
-         *
-         * @throws InvalidICalendarException when the iCalendar can't be parsed
-         * @throws IOException on I/O errors
-         */
-        fun tasksFromReader(reader: Reader): List<Task> {
-            val ical = fromReader(reader)
-            val vToDos = ical.getComponents<VToDo>(Component.VTODO)
-            return vToDos.mapTo(LinkedList()) { this.fromVToDo(it) }
-        }
-
-        private fun fromVToDo(todo: VToDo): Task {
-            val t = Task()
-
-            if (todo.uid != null)
-                t.uid = todo.uid.value
-            else {
-                logger.warning("Received VTODO without UID, generating new one")
-                t.generateUID()
-            }
-
-            // sequence must only be null for locally created, not-yet-synchronized events
-            t.sequence = 0
-
-            for (prop in todo.properties)
-                when (prop) {
-                    is Sequence -> t.sequence = prop.sequenceNo
-                    is Created -> t.createdAt = prop.dateTime.time
-                    is LastModified -> t.lastModified = prop.dateTime.time
-                    is Summary -> t.summary = prop.value
-                    is Location -> t.location = prop.value
-                    is Geo -> t.geoPosition = prop
-                    is Description -> t.description = prop.value
-                    is Color -> t.color = Css3Color.fromString(prop.value)?.argb
-                    is Url -> t.url = prop.value
-                    is Organizer -> t.organizer = prop
-                    is Priority -> t.priority = prop.level
-                    is Clazz -> t.classification = prop
-                    is Status -> t.status = prop
-                    is Due -> { t.due = prop }
-                    is Duration -> t.duration = prop
-                    is DtStart -> { t.dtStart = prop }
-                    is Completed -> { t.completedAt = prop }
-                    is PercentComplete -> t.percentComplete = prop.percentage
-                    is RRule -> t.rRule = prop
-                    is RDate -> t.rDates += prop
-                    is ExDate -> t.exDates += prop
-                    is Categories ->
-                        for (category in prop.categories)
-                            t.categories += category
-                    is Comment -> t.comment = prop.value
-                    is RelatedTo -> t.relatedTo.add(prop)
-                    is Uid, is ProdId, is DtStamp -> { /* don't save these as unknown properties */ }
-                    else -> t.unknownProperties += prop
-                }
-
-            t.alarms.addAll(todo.alarms)
-
-            // There seem to be many invalid tasks out there because of some defect clients, do some validation.
-            val dtStart = t.dtStart
-            val due = t.due
-
-            if (dtStart != null && due != null) {
-                if (DateUtils.isDate(dtStart) && DateUtils.isDateTime(due)) {
-                    logger.warning("DTSTART is DATE but DUE is DATE-TIME, rewriting DTSTART to DATE-TIME")
-                    t.dtStart = DtStart(DateTime(dtStart.value, due.timeZone))
-                } else if (DateUtils.isDateTime(dtStart) && DateUtils.isDate(due)) {
-                    logger.warning("DTSTART is DATE-TIME but DUE is DATE, rewriting DUE to DATE-TIME")
-                    t.due = Due(DateTime(due.value, dtStart.timeZone))
-                }
-
-
-                if (due.date <= dtStart.date) {
-                    logger.warning("Found invalid DUE <= DTSTART; dropping DTSTART")
-                    t.dtStart = null
-                }
-            }
-
-            if (t.duration != null && t.dtStart == null) {
-                logger.warning("Found DURATION without DTSTART; ignoring")
-                t.duration = null
-            }
-
-            return t
-        }
 
     }
 
