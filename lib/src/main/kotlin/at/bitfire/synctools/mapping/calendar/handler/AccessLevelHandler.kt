adc6770c47ed7b1a809fea9836e1f99e269804ba
Ricki Hirner <hirner@bitfire.at>
2026-02-21 14:58:29 +0100

    [WIP] Fixed AccessLevelHandler.kt

--- lib/src/main/kotlin/at/bitfire/synctools/mapping/calendar/handler/AccessLevelHandler.kt
+++ lib/src/main/kotlin/at/bitfire/synctools/mapping/calendar/handler/AccessLevelHandler.kt
@@ -17,41 +17,11 @@ import org.json.JSONException
 class AccessLevelHandler: AndroidEventFieldHandler {
 
     override fun process(from: Entity, main: Entity, to: VEvent) {
-        val values = from.entityValues
-
-        // take classification from main row
-        val classification = when (values.getAsInteger(Events.ACCESS_LEVEL)) {
-            Events.ACCESS_PUBLIC ->
-                Clazz.PUBLIC
-
-            Events.ACCESS_PRIVATE ->
-                Clazz.PRIVATE
-
-            Events.ACCESS_CONFIDENTIAL ->
-                Clazz.CONFIDENTIAL
-
-            else /* Events.ACCESS_DEFAULT */ ->
-                retainedClassification(from)
-        }
-        if (classification != null)
-            to.properties += classification
+        TODO("ical4j 4.x")
     }
 
     private fun retainedClassification(from: Entity): Clazz? {
-        val extendedProperties = from.subValues.filter { it.uri == ExtendedProperties.CONTENT_URI }.map { it.values }
-        val unknownProperties = extendedProperties.filter { it.getAsString(ExtendedProperties.NAME) == UnknownProperty.CONTENT_ITEM_TYPE }
-        val retainedClassification: Clazz? = unknownProperties.firstNotNullOfOrNull {
-            val json = it.getAsString(ExtendedProperties.VALUE)
-            val prop = try {
-                UnknownProperty.fromJsonString(json)
-            } catch (_: JSONException) {
-                // not parseable
-                null
-            }
-            prop as? Clazz
-        }
-
-        return retainedClassification
+        TODO("ical4j 4.x")
     }
 
 }
