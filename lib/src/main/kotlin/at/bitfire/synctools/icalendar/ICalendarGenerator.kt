75ae662665d3413eb5af1f55806fc90dd0b34b84
Ricki Hirner <hirner@bitfire.at>
2026-02-21 14:31:04 +0100

    [WIP] Fixed file ICalendarGenerator.kt

--- lib/src/main/kotlin/at/bitfire/synctools/icalendar/ICalendarGenerator.kt
+++ lib/src/main/kotlin/at/bitfire/synctools/icalendar/ICalendarGenerator.kt
@@ -34,59 +34,11 @@ class ICalendarGenerator {
      * @param to        stream that the iCalendar is written to
      */
     fun write(event: AssociatedComponents<*>, @WillNotClose to: Writer) {
-        val ical = Calendar()
-        ical.properties += Version.VERSION_2_0
-
-        // add PRODID
-        if (event.prodId != null)
-            ical.properties += event.prodId
-
-        // keep record of used timezones and earliest DTSTART to generate minified VTIMEZONEs
-        var earliestStart: Date? = null
-        val usedTimeZones = mutableSetOf<TimeZone>()
-
-        // add main event
-        if (event.main != null) {
-            ical.components += event.main
-
-            earliestStart = event.main.getProperty<DtStart>(Property.DTSTART)?.date
-            usedTimeZones += timeZonesOf(event.main)
-        }
-
-        // recurrence exceptions
-        for (exception in event.exceptions) {
-            ical.components += exception
-
-            exception.getProperty<DtStart>(Property.DTSTART)?.date?.let { start ->
-                if (earliestStart == null || start <= earliestStart)
-                    earliestStart = start
-            }
-            usedTimeZones += timeZonesOf(exception)
-        }
-
-        // add VTIMEZONE components
-        for (tz in usedTimeZones)
-            ical.components += ICalendar.minifyVTimeZone(tz.vTimeZone, earliestStart)
-
-        CalendarOutputter(false).output(ical, to)
+        TODO("ical4j 4.x")
     }
 
     private fun timeZonesOf(component: CalendarComponent): Set<TimeZone> {
-        val timeZones = mutableSetOf<TimeZone>()
-
-        // properties
-        timeZones += component.properties
-            .filterIsInstance<DateProperty>()
-            .mapNotNull { (it.date as? DateTime)?.timeZone }
-
-        // properties of subcomponents (alarms)
-        if (component is VEvent)
-            for (subcomponent in component.components)
-                timeZones += subcomponent.properties
-                    .filterIsInstance<DateProperty>()
-                    .mapNotNull { (it.date as? DateTime)?.timeZone }
-
-        return timeZones
+        TODO("ical4j 4.x")
     }
 
 }
