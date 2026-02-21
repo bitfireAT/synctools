62aab8e16148dc05d8864d0f9299ae7daf426e63
Ricki Hirner <hirner@bitfire.at>
2026-02-21 14:55:27 +0100

    [WIP] Fixed ColorHandler.kt

--- lib/src/main/kotlin/at/bitfire/synctools/mapping/calendar/handler/ColorHandler.kt
+++ lib/src/main/kotlin/at/bitfire/synctools/mapping/calendar/handler/ColorHandler.kt
@@ -19,23 +19,7 @@ class ColorHandler: AndroidEventFieldHandler {
         get() = Logger.getLogger(javaClass.name)
 
     override fun process(from: Entity, main: Entity, to: VEvent) {
-        val values = from.entityValues
-
-        // color can be specified as RGB value and/or as index key (CSS3 color of AndroidCalendar)
-        val color =
-            values.getAsString(Events.EVENT_COLOR_KEY)?.let { name ->      // try color key first
-                try {
-                    Css3Color.valueOf(name)
-                } catch (_: IllegalArgumentException) {
-                    logger.warning("Ignoring unknown color name \"$name\"")
-                    null
-                }
-            } ?: values.getAsInteger(Events.EVENT_COLOR)?.let { color ->        // otherwise, try to find the color name from the value
-                Css3Color.entries.firstOrNull { it.argb == color }
-            }
-
-        if (color != null)
-            to.properties += Color(null, color.name)
+        TODO("ical4j 4.x")
     }
 
 }
