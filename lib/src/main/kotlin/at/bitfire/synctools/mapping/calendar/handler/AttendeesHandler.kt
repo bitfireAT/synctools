2ece6f518443fb143133ca8d1c8ee86c52fc6ebe
Ricki Hirner <hirner@bitfire.at>
2026-02-21 14:57:41 +0100

    [WIP] Fixed AttendeesHandler.kt

--- lib/src/main/kotlin/at/bitfire/synctools/mapping/calendar/handler/AttendeesHandler.kt
+++ lib/src/main/kotlin/at/bitfire/synctools/mapping/calendar/handler/AttendeesHandler.kt
@@ -27,49 +27,11 @@ class AttendeesHandler: AndroidEventFieldHandler {
         get() = Logger.getLogger(javaClass.name)
 
     override fun process(from: Entity, main: Entity, to: VEvent) {
-        for (row in from.subValues.filter { it.uri == Attendees.CONTENT_URI })
-            populateAttendee(row.values, to)
+        TODO("ical4j 4.x")
     }
 
     private fun populateAttendee(row: ContentValues, to: VEvent) {
-        logger.log(Level.FINE, "Read event attendee from calendar provider", row)
-
-        try {
-            val attendee: Attendee
-            val email = row.getAsString(Attendees.ATTENDEE_EMAIL)
-            val idNS = row.getAsString(Attendees.ATTENDEE_ID_NAMESPACE)
-            val id = row.getAsString(Attendees.ATTENDEE_IDENTITY)
-
-            if (idNS != null || id != null) {
-                // attendee identified by namespace and ID
-                attendee = Attendee(URI(idNS, id, null))
-                email?.let { attendee.parameters.add(Email(it)) }
-            } else
-                // attendee identified by email address
-                attendee = Attendee(URI("mailto", email, null))
-            val params = attendee.parameters
-
-            // always add RSVP (offer attendees to accept/decline)
-            params.add(Rsvp.TRUE)
-
-            row.getAsString(Attendees.ATTENDEE_NAME)?.let { cn -> params.add(Cn(cn)) }
-
-            // type/relation mapping is complex and thus outsourced to AttendeeMappings
-            AttendeeMappings.androidToICalendar(row, attendee)
-
-            // status
-            when (row.getAsInteger(Attendees.ATTENDEE_STATUS)) {
-                Attendees.ATTENDEE_STATUS_INVITED -> params.add(PartStat.NEEDS_ACTION)
-                Attendees.ATTENDEE_STATUS_ACCEPTED -> params.add(PartStat.ACCEPTED)
-                Attendees.ATTENDEE_STATUS_DECLINED -> params.add(PartStat.DECLINED)
-                Attendees.ATTENDEE_STATUS_TENTATIVE -> params.add(PartStat.TENTATIVE)
-                Attendees.ATTENDEE_STATUS_NONE -> { /* no information, don't add PARTSTAT */ }
-            }
-
-            to.properties += attendee
-        } catch (e: URISyntaxException) {
-            logger.log(Level.WARNING, "Couldn't parse attendee information, ignoring", e)
-        }
+        TODO("ical4j 4.x")
     }
 
 }
