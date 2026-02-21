ccd1a67e3324a763c8258a0b47042a7aa170f661
Ricki Hirner <hirner@bitfire.at>
2026-02-21 14:22:52 +0100

    [WIP] Fixed file TaskReader.kt

--- lib/src/main/kotlin/at/bitfire/ical4android/TaskReader.kt
+++ lib/src/main/kotlin/at/bitfire/ical4android/TaskReader.kt
@@ -71,78 +71,7 @@ class TaskReader {
     }
 
     private fun fromVToDo(todo: VToDo): Task {
-        val t = Task()
-
-        if (todo.uid != null)
-            t.uid = todo.uid.value
-        else {
-            logger.warning("Received VTODO without UID, generating new one")
-            t.generateUID()
-        }
-
-        // sequence must only be null for locally created, not-yet-synchronized events
-        t.sequence = 0
-
-        for (prop in todo.properties)
-            when (prop) {
-                is Sequence -> t.sequence = prop.sequenceNo
-                is Created -> t.createdAt = prop.dateTime.time
-                is LastModified -> t.lastModified = prop.dateTime.time
-                is Summary -> t.summary = prop.value
-                is Location -> t.location = prop.value
-                is Geo -> t.geoPosition = prop
-                is Description -> t.description = prop.value
-                is Color -> t.color = Css3Color.fromString(prop.value)?.argb
-                is Url -> t.url = prop.value
-                is Organizer -> t.organizer = prop
-                is Priority -> t.priority = prop.level
-                is Clazz -> t.classification = prop
-                is Status -> t.status = prop
-                is Due -> { t.due = prop }
-                is Duration -> t.duration = prop
-                is DtStart -> { t.dtStart = prop }
-                is Completed -> { t.completedAt = prop }
-                is PercentComplete -> t.percentComplete = prop.percentage
-                is RRule -> t.rRule = prop
-                is RDate -> t.rDates += prop
-                is ExDate -> t.exDates += prop
-                is Categories ->
-                    for (category in prop.categories)
-                        t.categories += category
-                is Comment -> t.comment = prop.value
-                is RelatedTo -> t.relatedTo.add(prop)
-                is Uid, is ProdId, is DtStamp -> { /* don't save these as unknown properties */ }
-                else -> t.unknownProperties += prop
-            }
-
-        t.alarms.addAll(todo.alarms)
-
-        // There seem to be many invalid tasks out there because of some defect clients, do some validation.
-        val dtStart = t.dtStart
-        val due = t.due
-
-        if (dtStart != null && due != null) {
-            if (DateUtils.isDate(dtStart) && DateUtils.isDateTime(due)) {
-                logger.warning("DTSTART is DATE but DUE is DATE-TIME, rewriting DTSTART to DATE-TIME")
-                t.dtStart = DtStart(DateTime(dtStart.value, due.timeZone))
-            } else if (DateUtils.isDateTime(dtStart) && DateUtils.isDate(due)) {
-                logger.warning("DTSTART is DATE-TIME but DUE is DATE, rewriting DUE to DATE-TIME")
-                t.due = Due(DateTime(due.value, dtStart.timeZone))
-            }
-
-
-            if (due.date <= dtStart.date) {
-                logger.warning("Found invalid DUE <= DTSTART; dropping DTSTART")
-                t.dtStart = null
-            }
-        }
-
-        if (t.duration != null && t.dtStart == null) {
-            logger.warning("Found DURATION without DTSTART; ignoring")
-            t.duration = null
-        }
-
-        return t
+        TODO("ical4j 4.x")
     }
 
 }
