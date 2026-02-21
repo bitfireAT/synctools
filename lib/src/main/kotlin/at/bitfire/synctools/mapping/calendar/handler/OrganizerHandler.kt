1dac98b0ca2f1e5094789b66878a2b59ff350721
Ricki Hirner <hirner@bitfire.at>
2026-02-21 14:51:22 +0100

    [WIP] Fixed OrganizerHandler.kt

--- lib/src/main/kotlin/at/bitfire/synctools/mapping/calendar/handler/OrganizerHandler.kt
+++ lib/src/main/kotlin/at/bitfire/synctools/mapping/calendar/handler/OrganizerHandler.kt
@@ -22,18 +22,7 @@ class OrganizerHandler: AndroidEventFieldHandler {
         get() = Logger.getLogger(javaClass.name)
 
     override fun process(from: Entity, main: Entity, to: VEvent) {
-        // In case of an exception, we're taking ORGANIZER information from the main event and not the exception.
-        // See also RFC 6638 3.1 and 3.2.4.2.
-        val mainValues = main.entityValues
-
-        // ORGANIZER must only be set for group-scheduled events (= events with attendees)
-        val hasAttendees = from.subValues.any { it.uri == Attendees.CONTENT_URI }
-        if (hasAttendees && mainValues.containsKey(Events.ORGANIZER))
-            try {
-                to.properties += Organizer(URI("mailto", mainValues.getAsString(Events.ORGANIZER), null))
-            } catch (e: URISyntaxException) {
-                logger.log(Level.WARNING, "Error when creating ORGANIZER mailto URI, ignoring", e)
-            }
+        TODO("ical4j 4.x")
     }
 
 }
