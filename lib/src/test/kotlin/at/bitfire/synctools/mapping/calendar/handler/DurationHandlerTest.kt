90e7b36cf4a1772fd30bca5d5e72a1081e655856
Sunik Kupfer <kupfer@bitfire.at>
2026-03-17 13:49:35 +0100

    Update DurationHandler and tests

--- lib/src/test/kotlin/at/bitfire/synctools/mapping/calendar/handler/DurationHandlerTest.kt
+++ lib/src/test/kotlin/at/bitfire/synctools/mapping/calendar/handler/DurationHandlerTest.kt
@@ -9,34 +9,29 @@ package at.bitfire.synctools.mapping.calendar.handler
 import android.content.Entity
 import android.provider.CalendarContract.Events
 import androidx.core.content.contentValuesOf
+import at.bitfire.synctools.icalendar.dtEnd
 import junit.framework.TestCase.assertEquals
-import net.fortuna.ical4j.model.Date
-import net.fortuna.ical4j.model.DateTime
-import net.fortuna.ical4j.model.TimeZoneRegistryFactory
 import net.fortuna.ical4j.model.component.VEvent
 import net.fortuna.ical4j.model.property.DtEnd
 import org.junit.Assert.assertNull
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.temporal.Temporal
 
-@Ignore("ical4j 4.x")
 @RunWith(RobolectricTestRunner::class)
 class DurationHandlerTest {
 
-    private val tzRegistry = TimeZoneRegistryFactory.getInstance().createRegistry()
-    private val tzVienna = tzRegistry.getTimeZone("Europe/Vienna")!!
+    private val tzVienna = ZoneId.of("Europe/Vienna")
 
-    private val handler = DurationHandler(tzRegistry)
-
-    init {
-        TODO("ical4j 4.x")
-    }
+    private val handler = DurationHandler()
 
     // Note: When the calendar provider sets a non-null DURATION, it implies that the event is recurring.
 
-    /*@Test
+    @Test
     fun `All-day event with all-day duration`() {
         val result = VEvent()
         val entity = Entity(contentValuesOf(
@@ -45,7 +40,7 @@ class DurationHandlerTest {
             Events.DURATION to "P4D"
         ))
         handler.process(entity, entity, result)
-        assertEquals(DtEnd(Date("20200625")), result.endDate)
+        assertEquals(DtEnd(LocalDate.of(2020, 6, 25)), result.dtEnd<LocalDate>())
         assertNull(result.duration)
     }
 
@@ -58,7 +53,7 @@ class DurationHandlerTest {
             Events.DURATION to "P-4D"
         ))
         handler.process(entity, entity, result)
-        assertEquals(DtEnd(Date("20200625")), result.endDate)
+        assertEquals(DtEnd(LocalDate.of(2020, 6, 25)), result.dtEnd<LocalDate>())
         assertNull(result.duration)
     }
 
@@ -71,7 +66,7 @@ class DurationHandlerTest {
             Events.DURATION to "PT24H"
         ))
         handler.process(entity, entity, result)
-        assertEquals(DtEnd(Date("20251016")), result.endDate)
+        assertEquals(DtEnd(LocalDate.of(2025, 10, 16)), result.dtEnd<LocalDate>())
         assertNull(result.duration)
     }
 
@@ -84,7 +79,7 @@ class DurationHandlerTest {
             Events.DURATION to "PT-24H"
         ))
         handler.process(entity, entity, result)
-        assertEquals(DtEnd(Date("20251016")), result.endDate)
+        assertEquals(DtEnd(LocalDate.of(2025, 10, 16)), result.dtEnd<LocalDate>())
         assertNull(result.duration)
     }
 
@@ -99,7 +94,8 @@ class DurationHandlerTest {
         ))
         // DST transition at 03:00, clock is set back to 02:00 → P1D = PT25H
         handler.process(entity, entity, result)
-        assertEquals(DtEnd(DateTime("20251027T010000", tzVienna)), result.endDate)
+        val viennaDateTime = ZonedDateTime.of(2025, 10, 27, 1, 0, 0, 0, tzVienna)
+        assertEquals(DtEnd(viennaDateTime), result.dtEnd<ZonedDateTime>())
         assertNull(result.duration)
     }
 
@@ -114,7 +110,8 @@ class DurationHandlerTest {
         ))
         // DST transition at 03:00, clock is set back to 02:00 → P1D = PT25H
         handler.process(entity, entity, result)
-        assertEquals(DtEnd(DateTime("20251027T010000", tzVienna)), result.endDate)
+        val viennaDateTime = ZonedDateTime.of(2025, 10, 27, 1, 0, 0, 0, tzVienna)
+        assertEquals(DtEnd(viennaDateTime), result.dtEnd<ZonedDateTime>())
         assertNull(result.duration)
     }
 
@@ -129,7 +126,8 @@ class DurationHandlerTest {
         ))
         // DST transition at 03:00, clock is set back to 02:00 → PT24H goes one hour back
         handler.process(entity, entity, result)
-        assertEquals(DtEnd(DateTime("20251027T000000", tzVienna)), result.endDate)
+        val viennaDateTime = ZonedDateTime.of(2025, 10, 27, 0, 0, 0, 0, tzVienna)
+        assertEquals(DtEnd(viennaDateTime), result.dtEnd<ZonedDateTime>())
         assertNull(result.duration)
     }
 
@@ -144,7 +142,8 @@ class DurationHandlerTest {
         ))
         // DST transition at 03:00, clock is set back to 02:00 → PT24H goes one hour back
         handler.process(entity, entity, result)
-        assertEquals(DtEnd(DateTime("20251027T000000", tzVienna)), result.endDate)
+        val viennaDateTime = ZonedDateTime.of(2025, 10, 27, 0, 0, 0, 0, tzVienna)
+        assertEquals(DtEnd(viennaDateTime), result.dtEnd<ZonedDateTime>())
         assertNull(result.duration)
     }
 
@@ -181,8 +180,8 @@ class DurationHandlerTest {
             Events.EVENT_TIMEZONE to "Europe/Vienna"
         ))
         handler.process(entity, entity, result)
-        assertNull(result.endDate)
+        assertNull(result.dtEnd<Temporal>())
         assertNull(result.duration)
-    }*/
+    }
 
 }
