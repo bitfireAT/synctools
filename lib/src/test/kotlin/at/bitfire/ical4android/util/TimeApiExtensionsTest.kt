d6d517e3de18e4eec13f40c802b5a1290a8c703f
cketti <ck@cketti.de>
2026-03-24 17:40:45 +0100

    Remove unused functions from `TimeApiExtensions`

--- lib/src/test/kotlin/at/bitfire/ical4android/util/TimeApiExtensionsTest.kt
+++ lib/src/test/kotlin/at/bitfire/ical4android/util/TimeApiExtensionsTest.kt
@@ -7,175 +7,23 @@
 package at.bitfire.ical4android.util
 
 import at.bitfire.ical4android.util.TimeApiExtensions.abs
-import at.bitfire.ical4android.util.TimeApiExtensions.requireTimeZone
 import at.bitfire.ical4android.util.TimeApiExtensions.toDuration
-import at.bitfire.ical4android.util.TimeApiExtensions.toIcal4jDate
-import at.bitfire.ical4android.util.TimeApiExtensions.toIcal4jDateTime
 import at.bitfire.ical4android.util.TimeApiExtensions.toLocalDate
-import at.bitfire.ical4android.util.TimeApiExtensions.toLocalTime
 import at.bitfire.ical4android.util.TimeApiExtensions.toRfc5545Duration
-import at.bitfire.ical4android.util.TimeApiExtensions.toZoneIdCompat
-import at.bitfire.ical4android.util.TimeApiExtensions.toZonedDateTime
-import net.fortuna.ical4j.model.Date
-import net.fortuna.ical4j.model.DateTime
-import net.fortuna.ical4j.model.TimeZone
-import net.fortuna.ical4j.model.TimeZoneRegistryFactory
-import net.fortuna.ical4j.util.TimeZones
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
 import org.junit.Assert.fail
 import org.junit.Test
-import java.time.DayOfWeek
 import java.time.Duration
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.LocalTime
 import java.time.OffsetDateTime
 import java.time.Period
-import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import java.time.chrono.JapaneseDate
 
 class TimeApiExtensionsTest {
-
-    val tzRegistry = TimeZoneRegistryFactory.getInstance().createRegistry()!!
-    val tzBerlin = tzRegistry.getTimeZone("Europe/Berlin")!!
-
-
-    @Test
-    fun testTimeZone_toZoneIdCompat_NotUtc() {
-        assertEquals(ZoneId.of("Europe/Berlin"), tzBerlin.toZoneId())
-    }
-
-    @Test
-    fun testTimeZone_toZoneIdCompat_Utc() {
-        assertEquals(ZoneOffset.UTC, TimeZones.getUtcTimeZone().toZoneIdCompat())
-    }
-
-
-    @Test
-    fun testDate_toLocalDate() {
-        val date = Date("20200620").toLocalDate()
-        assertEquals(2020, date.year)
-        assertEquals(6, date.monthValue)
-        assertEquals(20, date.dayOfMonth)
-        assertEquals(DayOfWeek.SATURDAY, date.dayOfWeek)
-    }
-
-
-    @Test
-    fun testDateTime_requireTimeZone() {
-        val time = DateTime("2020707T010203", tzBerlin)
-        assertEquals(tzBerlin, time.requireTimeZone())
-    }
-
-    @Test
-    fun testDateTime_requireTimeZone_Floating() {
-        val time = DateTime("2020707T010203")
-        assertEquals(TimeZone.getDefault(), time.requireTimeZone())
-    }
-
-    @Test
-    fun testDateTime_requireTimeZone_Utc() {
-        val time = DateTime("2020707T010203Z").apply { isUtc = true }
-        assertTrue(time.isUtc)
-        assertEquals(TimeZones.getUtcTimeZone(), time.requireTimeZone())
-    }
-
-
-    @Test
-    fun testDateTime_toLocalDate_TimezoneBoundary() {
-        val date = DateTime("20200620T000000", tzBerlin).toLocalDate()
-        assertEquals(2020, date.year)
-        assertEquals(6, date.monthValue)
-        assertEquals(20, date.dayOfMonth)
-        assertEquals(DayOfWeek.SATURDAY, date.dayOfWeek)
-    }
-
-    @Test
-    fun testDateTime_toLocalDate_TimezoneDuringDay() {
-        val date = DateTime("20200620T123000", tzBerlin).toLocalDate()
-        assertEquals(2020, date.year)
-        assertEquals(6, date.monthValue)
-        assertEquals(20, date.dayOfMonth)
-        assertEquals(DayOfWeek.SATURDAY, date.dayOfWeek)
-    }
-
-    @Test
-    fun testDateTime_toLocalDate_UtcDuringDay() {
-        val date = DateTime("20200620T123000Z").apply { isUtc = true }.toLocalDate()
-        assertEquals(2020, date.year)
-        assertEquals(6, date.monthValue)
-        assertEquals(20, date.dayOfMonth)
-        assertEquals(DayOfWeek.SATURDAY, date.dayOfWeek)
-    }
-
-
-    @Test
-    fun testDateTime_toLocalTime() {
-        assertEquals(LocalTime.of(12, 30), DateTime("20200620T123000", tzBerlin).toLocalTime())
-    }
-
-    @Test
-    fun testDateTime_toLocalTime_Floating() {
-        assertEquals(LocalTime.of(12, 30), DateTime("20200620T123000").toLocalTime())
-    }
-
-    @Test
-    fun testDateTime_toLocalTime_Utc() {
-        assertEquals(LocalTime.of(12, 30), DateTime("20200620T123000Z").apply { isUtc = true }.toLocalTime())
-    }
-
-
-    @Test
-    fun testDateTime_toZonedDateTime() {
-        assertEquals(
-                ZonedDateTime.of(2020, 7, 7, 10, 30, 0, 0, tzBerlin.toZoneIdCompat()),
-                DateTime("20200707T103000", tzBerlin).toZonedDateTime()
-        )
-    }
-
-    @Test
-    fun testDateTime_toZonedDateTime_Floating() {
-        assertEquals(
-                ZonedDateTime.of(2020, 7, 7, 10, 30, 0, 0, ZoneId.systemDefault()),
-                DateTime("20200707T103000").toZonedDateTime()
-        )
-    }
-
-    @Test
-    fun testDateTime_toZonedDateTime_UTC() {
-        assertEquals(
-                ZonedDateTime.of(2020, 7, 7, 10, 30, 0, 0, ZoneOffset.UTC),
-                DateTime("20200707T103000Z").apply { isUtc = true }.toZonedDateTime()
-        )
-    }
-
-
-    @Test
-    fun testLocalDate_toIcal4jDate() {
-        assertEquals(Date("19000118"), LocalDate.of(1900, 1, 18).toIcal4jDate())
-        assertEquals(Date("20200620"), LocalDate.of(2020, 6, 20).toIcal4jDate())
-    }
-
-    @Test
-    fun testZonedDateTime_toIcal4jDateTime_NotUtc() {
-        assertEquals(
-            DateTime("20200705T010203", tzBerlin),
-            ZonedDateTime.of(2020, 7, 5, 1, 2, 3, 0, ZoneId.of("Europe/Berlin")).toIcal4jDateTime(tzRegistry)
-        )
-    }
-
-    @Test
-    fun testZonedDateTime_toIcal4jDateTime_Utc() {
-        assertEquals(
-            DateTime("20200705T010203Z"),
-            ZonedDateTime.of(2020, 7, 5, 1, 2, 3, 0, ZoneOffset.UTC).toIcal4jDateTime(tzRegistry)
-        )
-    }
-
 
     @Test
     fun testTemporalAmount_abs_Duration_negative() {
