c7f6bb21d51fa680d9945d2310eece1f2fb0dae4
cketti <ck@cketti.de>
2026-05-19 16:46:55 +0200

    [jtx] Add `ResourcesBuilder`

--- lib/src/main/kotlin/at/bitfire/synctools/mapping/jtx/JtxObjectBuilder.kt
+++ lib/src/main/kotlin/at/bitfire/synctools/mapping/jtx/JtxObjectBuilder.kt
@@ -16,6 +16,7 @@ import at.bitfire.synctools.mapping.jtx.builder.DescriptionBuilder
 import at.bitfire.synctools.mapping.jtx.builder.JtxEntityBuilder
 import at.bitfire.synctools.mapping.jtx.builder.RecurrenceFieldsBuilder
 import at.bitfire.synctools.mapping.jtx.builder.RemindersBuilder
+import at.bitfire.synctools.mapping.jtx.builder.ResourcesBuilder
 import at.bitfire.synctools.mapping.jtx.builder.SyncPropertiesBuilder
 import at.bitfire.synctools.mapping.jtx.builder.TimeFieldsBuilder
 import at.bitfire.synctools.storage.jtx.JtxObjectAndExceptions
@@ -43,7 +44,8 @@ class JtxObjectBuilder(
         TimeFieldsBuilder(),
         RemindersBuilder(),
         CategoriesBuilder(),
-        CommentsBuilder()
+        CommentsBuilder(),
+        ResourcesBuilder()
     )
 
     fun build(component: AssociatedComponents<CalendarComponent>): JtxObjectAndExceptions {
